15e5627227aa3dc613d8ea4f2b1cd934867b0d86
gagil <german.a.gil@gmail.com>
2017-07-14 15:30:57 -0600

    funcionalidad para test de selects
    se agrega funcionalidad para poder testear los metodos agregados en el dao-framework

--- src/main/java/org/gagl/core/servicio/GeneralService.java
+++ src/main/java/org/gagl/core/servicio/GeneralService.java
@@ -1,8 +1,15 @@
 package org.gagl.core.servicio;
 
+import java.util.List;
+
 import org.gagl.core.modelo.Persona;
 
 public interface GeneralService {
 	public void guardaPersona(Persona pEntity) throws Exception;
 	public void actualizaPersona(Persona pEntity) throws Exception;
+	public List<Persona> obtenerPersonas() throws Exception;
+	public List<Persona> obtenerPersonasPorParametro(String pNombre) throws Exception;
+	public List<Persona> obtenerPersonasPorParametroConQueryName(String pNombre) throws Exception;
+	public List<Persona> obtenerPersonasPorParametroConQueryNameAndClase(String pNombre) throws Exception;
+	public List<Persona> obtenerPersonasPorParametroPorSqlQuery(String pNombre) throws Exception;
 }
